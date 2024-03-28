5f40c3e76062167f53c42201df28c94b9681df63
jvazquez30 <josevazquez881@gmail.com>
2024-03-28 15:24:24 -0700

    New CSS changes

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,15 +1,35 @@
 import React from "react";
 import Figure from "./Figure";
+import styled from "styled-components";
+
+const StyledCard = styled.div`
+    border: 2px solid black;
+    border-radius: 12px;
+    padding: 2rem;
+
+
+    h2 {
+        color: royalblue;
+        font-size: 2em;
+    }
+    p {
+        margin:  0 0 1.5rem 0;
+        &::first-line {
+            font-size: 1.3em;
+        }
+    }
+
+`
 
 export default function Card({ title, text, imageURL, date }) {
     return (
-      <div className='card'>
+      <StyledCard className='card'>
         <h2>{title}</h2>
         <p>{text}</p>
         <Figure 
         imageURL={imageURL}
         dateOf={date}
         />
-      </div>
+      </StyledCard>
     )
   }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,10 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledFigure = styled.figure`
+max-width: 50%;
+
+figcaption {
+    font-style: italic;
+}
+
+`
 
 export default function Figure({ imageURL, dateOf }) {
     return (
-      <figure>
-        <img src={imageURL} />
-        <figcaption>Captured on {dateOf}</figcaption>
-      </figure>
+        <StyledFigure>
+            <img src={imageURL} />
+            <figcaption>Captured on {dateOf}</figcaption>
+        </StyledFigure>
     )
-  }
+}
